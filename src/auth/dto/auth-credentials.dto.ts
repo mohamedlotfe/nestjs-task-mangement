79af2e6330953f07632a74458b19cddcb28324bb
mohamedlotfe <mohamed.lotfy@contellect.com>
2021-05-23 13:36:18 +0200

    finish authentication step

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/,
+    { message: 'Password too Weak!' },
+  )
+  password: string;
+}
